8161038c1250cb4e3f197bd999088913efe5ba77
Sophia Sherman <sherman205@gmail.com>
2018-09-16 17:53:31 -0400

    Remove ejs architecture

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
 var app = express();
-app.set('view engine', 'ejs');
+//app.set('view engine', 'jsx');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use('/', router);
