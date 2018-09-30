5c1f5ddd4f1986b486d2e134047e06f391677243
Sophia Sherman <sherman205@gmail.com>
2018-09-30 17:25:31 -0400

    Connect to mongo database

--- server/models/blog.js
+++ server/models/blog.js
@@ -1,11 +1,10 @@
 //models/Blog.js
 var mongoose = require('mongoose');
+
 var Schema = mongoose.Schema;
 var blogSchema = new Schema({
   id: Number,
   title: String,
   content: String,
-});
+}, { timestamps: true });
 module.exports = mongoose.model('Blog', blogSchema);
-
-mongodb://<dbuser>:<dbpassword>@ds251287.mlab.com:51287/cerulean
--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 //server/server.js
+
 var express = require('express');
-var router = require('./routes/routes.js')
-var path = require('path');
+// var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
 var logger = require('morgan');
+
+var router = require('./routes/routes.js');
+var connectionString = require('./secrets')
+var Comment  = require('./models/blog');
+
 var API_PORT = process.env.API_PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
-// app.set('views', path.join(__dirname, '../client'));
-// app.use(express.static(path.join(__dirname, '../client')));
-// app.use(bodyParser.json({limit: '50mb'}));
-// app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
-// mongoose.connect('mongodb://test:macbook1@ds251287.mlab.com:51287/cerulean');
+
+mongoose.connect(connectionString, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.use('/api', router);
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
 module.exports=app;
